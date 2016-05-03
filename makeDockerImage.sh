1af0c5d874b09b69fe1a2fdbcf0e0db8c91fbb55
John Gossman <johngos@microsoft.com>
2016-05-03 09:48:30 -0700

    Changed band combination

--- makeDockerImage.sh
+++ makeDockerImage.sh
@@ -1,7 +1,25 @@
 #!/usr/bin/env bash
 set -xe 	
 
+PUSHIMAGE=0
+
+while getopts ":p" option
+do
+    case "$option" in
+    
+        p)
+            PUSHIMAGE=1
+            ;;
+    esac
+done
+
 # Statically link all packages so program is completely standalone and
 # works in a minimal Docker image
 CGO_ENABLED=0 GOOS=linux go build -v -a -installsuffix cgo .
-sudo docker build -t randelgo .
+
+if [ $PUSHIMAGE -ne "0" ]; then
+    sudo docker build -t gossmanster/randelgo .
+    sudo docker push gossmanster/randelgo
+else
+    sudo docker build -t randelgo .
+fi
--- randelbrot/mandelbrotService.go
+++ randelbrot/mandelbrotService.go
@@ -27,7 +27,7 @@ func RenderToBuffer(buffer *PixelBuffer, set *MandelbrotSet) {
 	renderer := new(Renderer)
 	maxCount := set.EstimateMaxCount()
 
-	bandMap := NewLogarithmicBandMap(maxCount, 32.0)
+	bandMap := NewLogarithmicBandMap(maxCount, 42.0)
 
 	renderer.RenderByCrawling(buffer, set, bandMap, maxCount)
 }
